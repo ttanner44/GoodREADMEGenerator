f69eb00a113b1fa42633b40805d37fe2d72e5e24
Tim Tanner <timtanner@tanner-companies.com>
2020-06-07 11:04:36 -0500

    Final Code

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# GoodREADMEGenerator
+
+  # ttanner44
+  [![GitHub license](https://img.shields.io/badge/license-MIT-Blue.svg)](CodeQuiz)
+  
+  ## Description
+  Code Quiz is a coding assessment game that renders a combination of multiple-choice questions and interactive challenges. It is timed use multiple-choice questions. The faster you answer, the higher your score.  But be careful, wrong answers will reduce time by 10 seconds. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code. It is built with a clean and polished user interface and is responsive, adapting to multiple screen sizes.
+  
+  https://ttanner44.github.io/CodeQuiz/
+  
+  ## Table of Contents
+  * [Usage](#Usage)
+  * [User Story](#User-Story)
+  * [Acceptance Criteria](#Acceptance-Criteria)
+  * [Installation](#installation)
+  * [License](#License)
+  * [Contributing](#Contributing)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+  
+  ## Usage
+  The user begins by clicking the Start Quiz button.  From there a 60 second clock begins to decrement.  5 questions are rendered.  The score is determined based upon the number of seconds remaining on the clock after all five questions are completed.  Each incorrect response decrements an extra 10 seconds.  A user can save their score with their initials.  High scores can be viewed by clicking the link on the top left at any time.
+  
+  ## User Story
+  AS A coding bootcamp student; I WANT to take a timed quiz on JavaScript fundamentals that stores high scores; SO THAT I can gauge my progress compared to my peers.
+  
+  ## Acceptance Criteria
+  GIVEN I am taking a code quiz; WHEN I click the start button, THEN a timer starts and I am presented with a question; WHEN I answer a question, THEN I am presented with another question; WHEN I answer a question incorrectly, THEN time is subtracted from the clock; WHEN all questions are answered or the timer reaches 0, THEN the game is over; WHEN the game is over, THEN I can save my initials and score.
+  
+  ## Installation
+  To install necessary dependencies, run the following command:
+  npm i 
+  
+  ## License
+  This project is licensed under the  MIT license.
+  
+  ## Contributing
+  All contributions are welcome.
+  
+  ## Tests
+  To run tests, run the following command:
+  npm test
+  
+  ## Questions
+  
+  If you have any quetions about the repo, open an issue or contact <img src="https://avatars2.githubusercontent.com/u/59519025?v=4" width="25" height="25"> directaly at timtanner@tanner-companies.com.
+  
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const axios = require("axios");
 const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -13,58 +14,164 @@ function promptUser() {
     },
     {
       type: "input",
+      name: "appUrl",
+      message: "What is the URL to run your application?"
+    },
+    {
+      type: "input",
+      name: "projectUrl",
+      message: "What is the URL to your project?"
+    },
+    {
+      type: "input",
       name: "repo",
-      message: "What is the project title?"
+      message: "What is the project name?"
     },
     {
       type: "input",
-      name: "install",
-      message: "What are the Installation Instructions?"
+      name: "description",
+      message: "Please write a short description of your project"
     },
     {
       type: "input",
-      name: "licenseType?",
+      name: "userStory",
+      message: "What is the user story?"
+    },
+    {
+      type: "input",
+      name: "accptCriteria",
+      message: "What are the acceptance criteria?"
+    },
+    {
+      type: "input",
+      name: "license",
       message: "What type of license?"
     },
     {
       type: "input",
-      name: "contributors",
-      message: "Who are the contributors?"
+      name: "install",
+      message: "What command should be run to install dependencies?"
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What command should be run to run tests?"
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using the application?"
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "What does the user need to know about contributing to the repo?"
     },
 
   ]);
 }
 
-function githubRepoData (answers.github, repo) {
-
-    const queryUrl = `https://api.github.com/users/${username}/${repo}`;
-    
-    axios.get(queryUrl).then(function(res) {
-        const repoNames = res.data.map(function(response1) {
-          return (response1.name, response1.avatar_url, response1.email)
-        });
-    }
-}
-
 function generateMD(answers) {
-//   return `write fiel`;
+  return `
+  # ${answers.github}
+  [![GitHub license](https://img.shields.io/badge/license-MIT-Blue.svg)](${answers.repo})
+  
+  ## Description
+  ${answers.description}
+  
+  ${answers.appUrl}
+  
+  ## Table of Contents
+  * [Usage](#Usage)
+  * [User Story](#User-Story)
+  * [Acceptance Criteria](#Acceptance-Criteria)
+  * [Installation](#installation)
+  * [License](#License)
+  * [Contributing](#Contributing)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## User Story
+  ${answers.userStory}
+  
+  ## Acceptance Criteria
+  ${answers.accptCriteria}
+  
+  ## Installation
+  To install necessary dependencies, run the following command:
+  ${answers.install} 
+  
+  ## License
+  This project is licensed under the  ${answers.license} license.
+  
+  ## Contributing
+  ${answers.contribution}
+  
+  ## Tests
+  To run tests, run the following command:
+  ${answers.tests}
+  
+  ## Questions
+  
+  If you have any quetions about the repo, open an issue or contact <img src="${answers.image}" width="25" height="25"> directaly at ${answers.email}.
+  `;
 }
 
-async function init() {
-  console.log("hi")
-  try {
-    const answers = await promptUser();
+promptUser()
 
-    const githubAPI = githubRepoData(answers);
-
-    const readmemd = generateMD(answers);
-
-    await writeFileAsync("readme.md", XXX);
-
-    console.log("Successfully wrote to readme.md");
-  } catch(err) {
+  .then(function(answers) {
+    const queryUrl = `https://api.github.com/users/${answers.github}`;
+    axios
+      .get(queryUrl)
+      .then(function(res) {
+        answers.image = res.data.avatar_url;
+        if (res.data.email !== null) {
+          answers.email = res.data.email;
+        } else {
+          answers.email = "timtanner@tanner-companies.com";
+        };
+        console.log(answers);
+        const data = generateMD(answers);
+        writeFileAsync("readme.md", data);
+        console.log("Successfully wrote to readme.md");
+      });
+  })
+  
+  .catch(function(err) {
     console.log(err);
-  }
-}
+  });
 
-init();
+
+// async function init() {
+//   console.log("hi")
+//   try {
+//     const answers = await promptUser();
+
+//     const queryUrl = `https://api.github.com/users/${answers.github}`;
+
+//     axios
+//       .get(queryUrl)
+//       .then(function(res) {
+//         answers.image = res.data.avatar_url;
+//         if (res.data.email !== null) {
+//           answers.email = res.data.email;
+//         } else {
+//           answers.email = "timtanner@tanner-companies.com";
+//         };
+//       });
+
+//     const data = generateMD(answers);
+
+//     await writeFileAsync("readme.md", data);
+
+//     console.log("Successfully wrote to readme.md");
+    
+//   } catch(err) {
+//     console.log(err);
+//   }
+// }
+
+// init();
